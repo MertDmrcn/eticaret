4bed949c46c2ff448e9f4322ef1bbdb356841b92
Mert Demircan <125318854+MertDmrcn@users.noreply.github.com>
2023-06-11 04:33:49 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// Scrool Bar Start
+window.addEventListener('scroll', function () {
+    var scrollBtn = document.getElementById('scrollBtn');
+    if (window.pageYOffset > 200) {
+        scrollBtn.classList.add('show-scroll-btn');
+    } else {
+        scrollBtn.classList.remove('show-scroll-btn');
+    }
+});
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // 
+    });
+}
+
+// Scrool Bar End
+//-----------------------------------------------------------------------------------
+//! Login Screen Start
+function toggleResetPswd(e) {
+    e.preventDefault();
+    $('#logreg-forms .form-signin').toggle() // display:block or none
+    $('#logreg-forms .form-reset').toggle() // display:block or none
+}
+
+function toggleSignUp(e) {
+    e.preventDefault();
+    $('#logreg-forms .form-signin').toggle(); // display:block or none
+    $('#logreg-forms .form-signup').toggle(); // display:block or none
+}
+
+$(() => {
+    // Login Register Form
+    $('#logreg-forms #forgot_pswd').click(toggleResetPswd);
+    $('#logreg-forms #cancel_reset').click(toggleResetPswd);
+    $('#logreg-forms #btn-signup').click(toggleSignUp);
+    $('#logreg-forms #cancel_signup').click(toggleSignUp);
+})
+
+//! Login Screen End
+
+
+// !Sipariş Ekranı Start
+
+var sipariş = document.getElementById("takipNo")
+
+// !Sipariş Ekranı Js End
